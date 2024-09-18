66634f345274f295c7d3650219627ccc37394852
StevenC-623 <130469793+StevenC-623@users.noreply.github.com>
2024-09-18 18:12:03 -0400

    stuff

--- Assets/03-MissionDemolision/Slingshot.cs
+++ Assets/03-MissionDemolision/Slingshot.cs
@@ -7,6 +7,7 @@ public class Slingshot : MonoBehaviour
 {
     [Header("Inscribed")]
     public GameObject projectilePrefab;
+    public float velocityMult = 10f;
 
     [Header("Dynamic")]
     public GameObject launchPoint;
@@ -37,5 +38,39 @@ public class Slingshot : MonoBehaviour
         projectile = Instantiate(projectilePrefab) as GameObject;
         projectile.transform.position = launchPos;
         projectile.GetComponent<Rigidbody>().isKinematic = true;
+    }
+    private void Update()
+    {
+        if (!aimingMode)
+        {
+            return;
+        }
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        Vector3 mouseDelta = mousePos3D - launchPos;
+
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if(mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            aimingMode = false;
+            Rigidbody projRB = projectile.GetComponent<Rigidbody>();
+            projRB.isKinematic = false;
+            projRB.collisionDetectionMode = CollisionDetectionMode.Continuous;
+            projRB.velocity = -mouseDelta * velocityMult;
+            FallowCam.POI = projectile;
+            projectile = null;
+        }
+
     }
 }
